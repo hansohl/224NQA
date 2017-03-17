735582257b3fc1df013923fbe82a15fa41bc005a
Thaminda Edirisooriya <tediris@stanford.edu>
2017-03-17 10:26:51 -0700

    Fixed dimensions in decoder, added some sizing comments to hmn

--- code/decoder.py
+++ code/decoder.py
@@ -1,26 +1,26 @@
 import tensorflow as tf
 
-def LSTMNode(h, c, u, scope, hidden_size = 200, l = 200):
+def LSTMNode(h, c, u, scope, hidden_size = 200):
     with tf.variable_scope(scope) as scope:
-        Wi = tf.get_variable("Wi", [2 * l, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
+        Wi = tf.get_variable("Wi", [4 * hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
         Ui = tf.get_variable("Ui", [hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
         # bi = tf.get_variable("bi", [hidden_size])
         # i = tf.sigmoid(tf.matmul(u, Wi) + tf.matmul(h, Ui) + bi)
         i = tf.sigmoid(tf.matmul(u, Wi) + tf.matmul(h, Ui))
 
-        Wf = tf.get_variable("Wf", [2 * l, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
+        Wf = tf.get_variable("Wf", [4 * hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
         Uf = tf.get_variable("Uf", [hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
         # bf = tf.get_variable("bf", initializer=tf.ones([batch_size, hidden_size]), dtype=tf.float32)
         # f = tf.sigmoid(tf.matmul(u, Wf) + tf.matmul(h, Uf) + bf)
         f = tf.sigmoid(tf.matmul(u, Wf) + tf.matmul(h, Uf))
 
-        Wo = tf.get_variable("Wo", [2 * l, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
+        Wo = tf.get_variable("Wo", [4 * hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
         Uo = tf.get_variable("Uo", [hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
         # bo = tf.get_variable("bo", [hidden_size])
         # o = tf.sigmoid(tf.matmul(u, Wo) + tf.matmul(h, Uo) + bo)
         o = tf.sigmoid(tf.matmul(u, Wo) + tf.matmul(h, Uo))
 
-        Wc = tf.get_variable("Wc", [2 * l, hidden_size], initializer=tf.contrib.layers.xavier_initializer())
+        Wc = tf.get_variable("Wc", [4 * hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer())
         Uc = tf.get_variable("Uc", [hidden_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer())
         # bc = tf.get_variable("bc", [hidden_size])
         # c_p = tf.tanh(tf.matmul(u, Wc) + tf.matmul(h, Uc) + bc)
@@ -36,7 +36,7 @@ class Decoder(object):
     def __init__(self, output_size):
         self.output_size = output_size
 
-    def decode(self, knowledge_rep, masks, batch_size, iters = 4, hidden_size = 200, l = 200):
+    def decode(self, knowledge_rep, masks, batch_size, iters = 4, hidden_size = 200):
         """
         takes in a knowledge representation
         and output a probability estimation over
@@ -68,7 +68,7 @@ class Decoder(object):
                 e = tf.constant(U[:, e_n, :])
 
                 u_se = tf.concat(1, (s_n, e_n))
-                h, c = LSTMNode(h, c, u_se, lstm_d)
+                h, c = LSTMNode(h, c, u_se, lstm_d, hidden_size)
 
 
             scope.reuse_variables()
--- code/hmn.py
+++ code/hmn.py
@@ -1,28 +1,30 @@
 POOL_SIZE = 16
 HIDDEN_LAYER_SIZE = 200
+batch_size = 10
+question_length = 600
 
 def HMN(U, h_i, s_prev, e_prev, scope_name):
     with tf.variable_scope(scope_name) as scope:
         # u_s = U[:,s_prev]
         # u_e = U[:,e_prev]
 
-        W1 = tf.get_variable("HMN_W1", [POOL_SIZE, HIDDEN_LAYER_SIZE, 3*HIDDEN_LAYER_SIZE], initializer=tf.contrib.layers.xavier_initializer())
+        W1 = tf.get_variable("HMN_W1", [3 * HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE, POOL_SIZE], initializer=tf.contrib.layers.xavier_initializer())
         W2 = tf.get_variable("HMN_W2", [POOL_SIZE, HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE], initializer=tf.contrib.layers.xavier_initializer())
         W3 = tf.get_variable("HMN_W3", [POOL_SIZE, 1, 2*HIDDEN_LAYER_SIZE], initializer=tf.contrib.layers.xavier_initializer())
-        Wd = tf.get_variable("HMN_WD", [HIDDEN_LAYER_SIZE, 5*HIDDEN_LAYER_SIZE], initializer=tf.contrib.layers.xavier_initializer())
+        Wd = tf.get_variable("HMN_WD", [5 * HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE], initializer=tf.contrib.layers.xavier_initializer())
 
-        b1 = tf.get_variable("HMN_B1", tf.zeros([POOL_SIZE, HIDDEN_LAYER_SIZE]))
-        b2 = tf.get_variable("HMN_B2", tf.zeros([POOL_SIZE, HIDDEN_LAYER_SIZE]))
-        b3 = tf.get_variable("HMN_B3", tf.zeros(POOL_SIZE))
+        b1 = tf.get_variable("HMN_B1", tf.zeros([HIDDEN_LAYER_SIZE, POOL_SIZE]))
+        b2 = tf.get_variable("HMN_B2", tf.zeros([HIDDEN_LAYER_SIZE, POOL_SIZE]))
+        b3 = tf.get_variable("HMN_B3", tf.zeros([POOL_SIZE]))
 
-        h_i_tiled = tf.tile(h_i, (1, batch_size)) #WHAT IS THE BATCH SIZE? Is h_i already a batch?
+        h_i_tiled = tf.tile(h_i, (1, batch_size)) #h is indeed a batch
         hsu = tf.concat(1, [h_i_tiled, s_prev, e_prev])
-
-        r = tf.nn.tanh(tf.matmul(hsu, Wd))
-        r_tiled = tf.tile(r, (1, m))
-        Ur = tf.concat(U, r_tiled)
-        m1 = tf.reduce_max(tf.matmul(Ur, W1) + b1, axis=0)
-        m2 = tf.reduce_max(tf.matmul(m1, W2) + b2, axis=0)
+        #hsu is batch_size x (hidden_size * 5)
+        r = tf.nn.tanh(tf.matmul(hsu, Wd)) # r is batch_size x hidden_size
+        r_tiled = tf.tile(r, (1, m)) #U is batch_size x question_length x (hidden_size * 2)
+        Ur = tf.concat(U, r_tiled) #Ur is batch_size x question_length x (hidden_size * 3)
+        m1 = tf.reduce_max(tf.matmul(Ur, W1) + b1, axis=0) #m1 is batch_size x question_length x hidden_size x pool_size
+        m2 = tf.reduce_max(tf.matmul(m1, W2) + b2, axis=0) #m2 is batch_size x question_length x hidden_size
 
         m1m2 = tf.concat(1, [m1, m2])
         hmn = tf.reduce_max(tf.matmul(m1m2, W3) + b3, axis=0)
